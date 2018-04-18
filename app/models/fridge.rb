48a5bd208a8a260e0fbd282b5d045c782de5276d
github username <github email address>
2018-04-18 16:30:23 -0400

    some additional methods to address user stories

--- app/models/fridge.rb
+++ app/models/fridge.rb
@@ -1,4 +1,9 @@
 class Fridge < ActiveRecord::Base
    has_many :items
    has_many :users, through: :items
+
+   def fridge_items(fridge_id)
+     Item.where(fridge_id: fridge_id)
+   end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ActiveRecord::Base
-    belongs_to :users  
+    belongs_to :users
     belongs_to :fridges
+
+    def get_all_instances_by_name(name)
+      Item.where(name: name)
+    end
+
+    def find_users_who_own_item(name)
+        items = get_all_instances_by_name(name)
+        items.map{|i| User.find_by_id(i.user_id)}
+    end
+
+    def find_fridges_who_with_item(name)
+      items = get_all_instances_by_name(name)
+      items.map{|i| Fridge.find_by_id(i.fridge_id)}
+    end
+    
+
  end
