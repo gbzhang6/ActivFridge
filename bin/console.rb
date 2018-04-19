e4ccf0635fbc1cd9dd9ca99575dd72bf13d2d446
Michael Myung <michael.h.myung@gmail.com>
2018-04-19 10:37:43 -0400

    Using Faker gem to populate seed file

--- bin/console.rb
+++ bin/console.rb
@@ -9,6 +9,4 @@ puts "\n"
 name = prompt.ask("Please enter your name:", default: ENV['USER'])
 current_user = User.find_or_create_by(name: name, status: "happy", pantry: "empty")
 
-
-
 #puts "Welcome, #{current_user.name}!"
